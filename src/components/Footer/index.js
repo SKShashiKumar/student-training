8119ee9a51857db45c61e73d5d336c0724e239a0
SKShashiKumar <shashimssk@gmail.com>
2022-02-21 20:29:54 +0530

    Completed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,10 +4,49 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <div className="Footer">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12">
-            <h1>Footer</h1>
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+            <form action="#">
+            <div className="mt-4">
+              <input type="text" className="form-control" placeholder="Enter your name"/>
+            </div>
+            <div className="mt-4">
+              <input type="email" className="form-control" placeholder="Enter valid email address"/>
+            </div>
+            <div className="mt-4">
+              <input type="tel" className="form-control" placeholder="Enter your phone number (+91 9876543210)"/>
+            </div>
+            <div className="mt-4">
+              <textarea className="form-control" rows="4"></textarea>
+            </div>
+            <div className="mt-4 submitButton">
+              <button>Submit</button>
+            </div>
+            </form>
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+            <div className="FirstRow">
+              <h5>Call us</h5>
+              <p>1 (234) 567-891 <br/>
+                  1 (234) 987-654</p>
+            </div>
+            <div className="SecondRow">
+              <h5>location</h5>
+              <p>121 Rock Sreet, 21 Avenue,New York, NY 92103-9000</p>
+            </div>
+            <div className="ThirdRow">
+              <h5>top services</h5>
+              <ul>
+                <li>Pre Design</li>
+                <li>Pre Design</li>
+                <li>Pre Design</li>
+                <li>Pre Design</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+            <img src="images/Footer.PNG" alt="Footer" />
           </div>
         </div>
       </div>
--- src/components/Testi/index.js
+++ src/components/Testi/index.js
@@ -29,43 +29,21 @@ const Testi = () => {
                         modules={[Navigation,Pagination]}
                         className="mySwiper"
                 >
-                    {/* <SwiperSlide>
-                       <div className="container-fluid Test1">
-                           <div className="row justify-content-start align-items-center">
-                               <div className="col-5">
-                                   <img src="images/Testinomial1.jpg" alt="Test1" />
-                               </div>
-                               <div className="col-7">
-                                   <div className="row">
-                                       <div className="col-12">
-                                           <h2>John Jackson,<span>developer</span></h2>
-                                       </div>
-                                       <div className="col-12">
-                                           <p>There are two types of people who will tell you that you cannot make a difference in this world: those who are afraid to try and those who are afraid you will succeed.</p>
-                                       </div>
-                                       <div className="col-12 readMoreButton">
-                                           <button>read more</button>
-                                       </div>
-                                   </div>
-                               </div>
-                           </div>
-                        </div>
-                    </SwiperSlide> */}
                     <SwiperSlide>
                        <div className="container-fluid Test1">
-                           <div className="row justify-content-start align-items-center">
-                               <div className="col-5">
+                           <div className="row align-items-center">
+                               <div className="col-xs-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 text-center">
                                    <img src="images/Testinomial1.jpg" alt="Test1" />
                                </div>
-                               <div className="col-7 ">
-                                   <div className="row justify-content-start align-items-center">
-                                       <div className="col-12">
+                               <div className="col-xs-12 col-sm-12 col-md-7 col-lg-7 col-xl-7 ">
+                                   <div className="row align-items-center">
+                                       <div className="col-12 pt-3 ">
                                        <h2>John Jackson,<span>developer</span></h2>
                                        </div>
-                                       <div className="col-12">
+                                       <div className="col-12 pt-3 ">
                                            <p>There are two types of people who will tell you that you cannot make a difference in this world: those who are afraid to try and those who are afraid you will succeed.</p>
                                        </div>
-                                       <div className="col-12 readMoreButton">
+                                       <div className="col-12 pt-3  readMoreButton">
                                            <button>read more</button>
                                        </div>
                                    </div>
@@ -75,19 +53,19 @@ const Testi = () => {
                     </SwiperSlide>
                     <SwiperSlide>
                        <div className="container-fluid Test1">
-                           <div className="row justify-content-start align-items-center">
-                               <div className="col-5">
+                           <div className="row align-items-center">
+                               <div className="col-xs-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 text-center">
                                    <img src="images/Linda.jpeg" alt="Test1" />
                                </div>
-                               <div className="col-7 ">
-                                   <div className="row justify-content-start align-items-center">
-                                       <div className="col-12">
+                               <div className="col-xs-12 col-sm-12 col-md-7 col-lg-7 col-xl-7 ">
+                                   <div className="row align-items-center">
+                                       <div className="col-12 pt-3 ">
                                            <h2>Linda Larson,<span>designer</span></h2>
                                        </div>
-                                       <div className="col-12">
+                                       <div className="col-12 pt-3 ">
                                            <p>There are two types of people who will tell you that you cannot make a difference in this world: those who are afraid to try and those who are afraid you will succeed.</p>
                                        </div>
-                                       <div className="col-12 readMoreButton">
+                                       <div className="col-12  pt-3 readMoreButton">
                                            <button>read more</button>
                                        </div>
                                    </div>
